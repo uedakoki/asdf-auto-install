a1d841d58c71a30ec6890b11c1259c9736b00e02
uedakoki <koki19970301@gmail.com>
2022-05-06 18:06:02 +0900

    [fix] file structure

--- src/asdf_auto_install/install.py
+++ src/asdf_auto_install/install.py
@@ -4,7 +4,8 @@ import sys
 import json
 from pathlib import Path
 
-from asdf_auto_install.plugin_cls import Plugin
+# from asdf_auto_install.plugin_cls import Plugin
+from cls import Plugin
 
 
 def get_args():
